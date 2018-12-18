78f9e285e4817a6b3e85f48947ab956a0a9a8e0f
Avery Biskup <averybiskup@gmail.com>
2018-12-18 14:49:50 -0800

    Moved info to readme

--- monty-hall/solve.py
+++ monty-hall/solve.py
@@ -1,25 +1,3 @@
-# The Monty Hall problem is from a game show where there
-# are three doors: 2 with goats (or something bad), and 1
-# with a car.
-# First: the player chooses one door
-# Second: the player is shown one of the doors with a goat in it
-# Third: the player is asked weather he/she wants to switch their choice
-# Fourth: whichever door the player ends with, he/she gets that prize
-#
-# The problem:
-# Is it best to switch your choice or keep with your door.
-#
-# Solution:
-# If you don't switch, you have 33% of getting the car
-#
-# If you do switch you have 66% of getting the car,
-# because you have seen 2 doors
-#
-# ALWAYS SWITCH
-
-#This program tests this theory with a brute force method
-
-# NOT PUSHED YET
 def play(withSwap):
 
     # Gets indexes for car and guess
