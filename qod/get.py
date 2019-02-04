a98b0f90bd378f5d96d35a08093bb65517520172
Avery Biskup <averybiskup@gmail.com>
2019-02-03 20:57:29 -0800

    Completed

--- qod/get.py
+++ qod/get.py
@@ -1,3 +1,13 @@
 import requests
 
 url = "http://quotes.rest/qod.json"
+
+r = requests.get(url)
+
+q = r.json()
+try:
+    text = q['contents']['quotes'][0]
+    print('"' + text['quote'] + '"\n\n\t- ' + text['author'])
+except:
+    msg = q['error']['message']
+    print(msg)
