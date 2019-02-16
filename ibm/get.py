8b5fbe8e302ab30931a3b585ca8850f664a98948
Avery Biskup <averybiskup@gmail.com>
2019-02-16 10:06:33 -0800

    treehacks19

---
+++ ibm/get.py
@@ -0,0 +1,24 @@
+import json
+from watson_developer_cloud import NaturalLanguageUnderstandingV1
+from watson_developer_cloud.natural_language_understanding_v1 \
+    import Features, EntitiesOptions, KeywordsOptions
+
+def process(key, text):
+    naturalLanguageUnderstanding = NaturalLanguageUnderstandingV1(
+        version='2018-11-16',
+        iam_apikey=key,
+        url='https://gateway.watsonplatform.net/natural-language-understanding/api')
+
+        # t = 'IBM is an American multinational technology company '
+        # 'headquartered in Armonk, New York, United States, '
+        # 'with operations in over 170 countries.'
+
+    t = text
+
+    response = naturalLanguageUnderstanding.analyze(text=t,
+    features=Features(
+        entities=EntitiesOptions(emotion=True, sentiment=True, limit=2),
+        keywords=KeywordsOptions(emotion=True, sentiment=True,
+        limit=2))).get_result()
+
+    return (json.dumps(response, indent=2))
