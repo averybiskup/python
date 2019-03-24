9af36b5cd333dbeca79ee9a1ffa869245d2a675e
Avery Biskup <averybiskup@gmail.com>
2019-03-24 10:28:52 -0700

    A little late for pie day

---
+++ test/list.py
@@ -0,0 +1,29 @@
+# li = []
+#
+# an = int(input('How many #: '))
+# # a = an
+# while (an > 0):
+#     tell = int(input('#: '))
+#     li.append(tell)
+#     an = an-1
+#     print(li)
+#
+#
+#
+# parse(an)
+#
+# ask = int(input('Which number would you like to see: '))
+#
+# # for x in range(0, a):
+# #     if ask == li[x]:
+# #         print(ask == li[x])
+#
+# print(li, an)
+# while(an > 0):
+#     print(ask == li[an-1])
+#     an -= an
+
+a = 5
+b = 4
+
+print("Hey the number is {}")
