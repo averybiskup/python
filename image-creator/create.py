7a9d7e156419a84319cad1896615654dadb4a318
Avery Biskup <averybiskup@gmail.com>
2019-11-19 19:02:29 -0800

    fixed quote

--- image-creator/create.py
+++ image-creator/create.py
@@ -1,15 +1,36 @@
 from get import request
+from set import set_image
 import textwrap
-
+import os
+import datetime
 from PIL import Image, ImageDraw, ImageFont
-img = Image.new('RGB', (2560, 1600), color = (73, 109, 137))
+import random
 
-fnt = ImageFont.truetype('/Library/Fonts/Arial.ttf', 150)
-d = ImageDraw.Draw(img)
 
-margin = offset = 30
-for line in textwrap.wrap(request(), width=30):
-    d.text((margin, offset), line, font=fnt, fill="#ffffff")
-    offset += fnt.getsize(line)[1]
+def random_color():
+    rgbl=[255,0,0]
+    random.shuffle(rgbl)
+    return tuple(rgbl)
 
-img.save('text.png')
+def new_image(text, size, bgcolor, textcolor, outfile, set):
+    img = Image.new('RGB', size, color = bgcolor)
+
+    fnt = ImageFont.truetype('/Library/Fonts/Arial.ttf', 100)
+    d = ImageDraw.Draw(img)
+
+    offset = 40
+    margin = 50
+    for line in textwrap.wrap(text, width=30):
+        d.text((margin, offset), line, font=fnt, fill = textcolor)
+        offset += fnt.getsize(line)[1]
+
+    filename = 'images/' + outfile + ".png"
+    img.save(filename)
+    if set:
+        set_image(filename)
+
+
+date = str(datetime.datetime.now().date())
+filename = date + '3'
+
+new_image(request(), (2560, 1600), random_color(), "#FFFFFF", filename, True)
