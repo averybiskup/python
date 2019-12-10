1c330078fab3d70901110ef6f34089a820b3b477
Avery Biskup <averybiskup@gmail.com>
2019-12-09 17:11:11 -0800

    wooh

---
+++ image-creator/removebg.py
@@ -0,0 +1,53 @@
+from PIL import Image, ImageMorph, ImagePalette
+import numpy as np
+
+img = Image.open('flower.jpg').convert('RGBA')
+
+x = img.size[0]
+y = img.size[1]
+
+data = np.array(img)
+rgb = data[:,:,:3]
+
+def near_color(col1, col2):
+    first = abs(col1[0] - col2[0])
+    second = abs(col1[1] - col2[1])
+    third = abs(col1[2] - col2[2])
+    return (first + second + third) <= 300
+
+out_im = Image.new('RGB', (x,y), (255,255,255))
+
+
+def change(color):
+
+    mask = np.all(near_color(rgb, color), axis = -1)
+    data[mask] = [0,0,0,255]
+
+black = (0,0,0,255)
+# img.getpixel((1571, 1032))
+pixels = img.load()
+new_pixels = out_im.load()
+for i in range(x-1):
+    for l in range(y-1):
+        # print(pixels[i, 0])
+        if near_color(pixels[i, l], black):
+                print(i, l)
+                # pixels[i, l] = black
+                new_pixels[i, l] = black
+
+new_im = img.convert('RGB')
+new_im.save('done.jpg')
+new_im.show()
+
+out_im.save('deep_fried.jpg')
+out_im.show()
+
+
+# for i in range(5):
+#     # print(i)
+#     for j in range(5):
+#         change(img.getpixel((j, i))[0:3])
+#
+# new_im = Image.fromarray(data).convert('RGB')
+# new_im.save('done.jpg')
+# new_im.show()
