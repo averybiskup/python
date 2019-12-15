d2a0a25fa596e8b94a3a464cafa0547842ff5ee8
Avery Biskup <averybiskup@gmail.com>
2019-12-15 14:41:00 -0800

    lookin cool

--- image-creator/removebg.py
+++ image-creator/removebg.py
@@ -13,9 +13,15 @@ def near_color(col1, col2):
     first = abs(col1[0] - col2[0])
     second = abs(col1[1] - col2[1])
     third = abs(col1[2] - col2[2])
-    return (first + second + third) <= 300
+    return (first + second + third) <= 130
 
-out_im = Image.new('RGB', (x,y), (255,255,255))
+blue = (86,128,156)
+black = (0,0,0,255)
+yellow = (255,213,0, 255)
+pink = (250, 108, 196, 255)
+gray = (130,130,130)
+
+out_im = Image.new('RGB', (x,y), gray)
 
 
 def change(color):
@@ -23,17 +29,18 @@ def change(color):
     mask = np.all(near_color(rgb, color), axis = -1)
     data[mask] = [0,0,0,255]
 
-black = (0,0,0,255)
+
 # img.getpixel((1571, 1032))
 pixels = img.load()
 new_pixels = out_im.load()
 for i in range(x-1):
     for l in range(y-1):
         # print(pixels[i, 0])
-        if near_color(pixels[i, l], black):
+        if near_color(pixels[i, l], pink):
                 print(i, l)
                 # pixels[i, l] = black
-                new_pixels[i, l] = black
+                new_pixels[i, l] = yellow
+                pixels[i, l] = yellow
 
 new_im = img.convert('RGB')
 new_im.save('done.jpg')
