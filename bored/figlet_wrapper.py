67e3e7e4c957643eace362be1a3522925785d9cb
Avery Biskup <averybiskup@gmail.com>
2020-05-31 10:43:42 -0700

    figlet wrapper progress

---
+++ bored/figlet_wrapper.py
@@ -0,0 +1,29 @@
+from pyfiglet import Figlet
+
+fc = {
+    'red': 31,
+    'green': 32,
+    'yellow': 33,
+    'blue': 34,
+    'purple': 35,
+    'cyan': 36,
+    'white': 37 
+ }
+
+fc = {
+    'red': 31,
+    'green': 32,
+    'yellow': 33,
+    'blue': 34,
+    'purple': 35,
+    'cyan': 36,
+    'white': 37
+ }
+
+
+def p(text, style, fc, bgc):
+    
+    f= Figlet(font=style)
+    str = '\33[1;{};{}m{}\33[1;37;40m'.format(fc, bgc, f.renderText(text))
+    
+    return str
