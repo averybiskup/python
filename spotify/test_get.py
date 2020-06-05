f03ece2b0e4d090b8581b2879122bafce801ac4a
Avery Biskup <averybiskup@gmail.com>
2020-06-04 22:26:44 -0700

    added followers function

--- spotify/test_get.py
+++ spotify/test_get.py
@@ -2,12 +2,21 @@ import os
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
+
+# Using spotify credentials to access API and returns the general info about artist
+def get_artist_info(name):
+    spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+    results = spotify.search(q='artist:' + name, type='artist')
+    items = results['artists']['items']
+    if len(items) > 0:
+        return items
+
+# Function for getting the background image of the artist
 def get_background(name):
+    items = get_artist_info(name)
+
     try: 
-        spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
-        results = spotify.search(q='artist:' + name, type='artist')
-        items = results['artists']['items']
-        
+                
         if len(items) > 0:
             artist = items[0]
             print(artist)
@@ -16,15 +25,14 @@ def get_background(name):
             return 'Something Went Wrong'
     except:
         return 'No Artist Found'
+
+# Function for getting the genres of the artist
 def get_genres(name):
-    
+    items = get_artist_info(name)
+
     try:
-        spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
-        results = spotify.search(q='artist:' + name, type='artist')
-        items = results['artists']['items']
         if len(items) > 0:
             artist = items[0]
-
             return artist['genres']
         else:
             return 'Something Went Wrong'
@@ -32,6 +40,20 @@ def get_genres(name):
     except:
         return 'Artist Not Found'
 
+# Function for getting the number of followers that the artist has
+def get_followers(name):
+    items = get_artist_info(name)
+
+    try:
+           if len(items) > 0:
+            artist = items[0]
+
+            return artist['followers']['total']
+        else:
+            return 'Something Went Wrong'
+
+    except:
+        return 'Artist Not Found'
 
 
-print(get_genres('Radiohead'))
+print(get_followers('Yung Lean'))
