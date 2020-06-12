819df9b41b8c5136cee74de6ae995ca8bb86742e
Avery Biskup <averybiskup@gmail.com>
2020-06-11 23:52:44 -0700

    working game loop

--- trivia/q.py
+++ trivia/q.py
@@ -1,19 +1,68 @@
 import requests
 import json
+import sys
+import os
+import time
+from figlet_wrapper import p
 
 r = requests.get('https://opentdb.com/api.php?amount=10')
 
-if r.status_code == 200:
-    questions = r.json()['results']
-    for q in questions:
-        question = q['question']
-        difficulty = q['difficulty']
-        correct = q['correct_answer']
-        incorrect = q['incorrect_answers']
-        category = q['category']
-        #print(q.keys())
-        answers = incorrect
-        answers.append(correct)
-        for answer in answers:
-            print(answer)
-        print('\n')
+os.system('clear')
+print(p('Welcome to Trivia!', 'slant', 'cyan'))
+time.sleep(2)
+
+if r.status_code != 200:
+    print('Something went wrong')
+    sys.exit()
+
+current = 0
+questions = r.json()['results']
+
+
+def list_to_obj(l):
+    obj = {}
+
+    for i, l in enumerate(l):
+        obj[i + 1] = l
+
+    return obj
+
+while current < len(questions):
+    os.system('clear')
+    q = questions[current]
+    question = q['question']
+    difficulty = q['difficulty']
+    correct = q['correct_answer']
+    incorrect = q['incorrect_answers']
+    category = q['category']
+    answers = incorrect
+    answers.append(correct)
+    
+    a_obj = list_to_obj(answers)
+    
+
+    print(question + '\n')
+
+    for n, a in a_obj.items():
+        print('[' + str(n) + ']', a)
+
+    user_a = int(input('>'))
+
+    os.system('clear')
+    if a_obj[user_a] == correct:
+        print(p('nice', 'r', 'green'))
+    else:
+        print(p('not nice', 'r', 'red'))
+        print(correct)
+
+    time.sleep(1)
+
+    current += 2
+
+print('Game Over')
+
+
+
+list_to_obj(['hi', 'no'])
+
+
