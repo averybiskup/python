f1b93c53c0b3367a1e560a7d4a59330621ea745e
Avery Biskup <averybiskup@gmail.com>
2021-10-04 15:22:58 -0700

    first commit to habit-pickler

---
+++ habit-pickler/README.md
@@ -0,0 +1,10 @@
+The idea for this project is to solidify habits.  
+  
+Pickling is the process of turning something raw, that can't  
+last long on it's own, to something that can last without external help.  
+  
+Much like pickling, forming habits takes time, and effort.  
+  
+Hence, habit-pickler.  
+  
+I give credit to this idea to the library I will be using: pickle.
---
+++ habit-pickler/pickle.py
@@ -0,0 +1,13 @@
+import pickle
+
+# TODO create jar.data for storing streak information
+# TODO create python script to interface between user and streak information
+# TODO create function for determining length of streaks
+# TODO create methods for pickling user-inputted streaks into jar.data
+
+def main():
+    print('Work in progress')
+    return 0
+
+if __name__ == '__main__':
+    main()
