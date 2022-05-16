878ebd5d04a469a744c0649e5dd2206f996df1bf
Avery Biskup <averybiskup@gmail.com>
2022-05-15 21:52:31 -0700

    comment

--- wiki/crawler.py
+++ wiki/crawler.py
@@ -1,40 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
-url = 'https://stackoverflow.com/questions/49170271/no-module-named-beautifulsoup4-in-python3'
+url = 'https://news.ycombinator.com/rss'
 
 r = requests.get(url)
 
-soup = BeautifulSoup(r.content, 'html.parser')
+soup = BeautifulSoup(r.content, 'lxml')
 
 anchors = soup.find_all('a')
-
-http = []
-https = []
-
-def trim(link):
-    count = 0
-    for i in range(0, len(link)):
-        if count == 3:
-            break
-        if link[i] == '/' or link[i] == '?':
-            count += 1
-
-    return link[0:i]
-
 
 for anchor in anchors:
     try:
         link = anchor['href']
-        if link[0:5] == 'https':
-            print(trim(link))
-            https.append(link)
-        else:
-            http.append(link)
+        print(link)
         
     except:
         pass
-print(len(http))
-print(len(https))
 
-
+# this seems to be impossible.. at least the idea I originally had
