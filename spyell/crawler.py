1ca5ecfe58348d8c2d9b5ac6af10007c40fcc130
Avery Biskup <averybiskup@gmail.com>
2022-06-07 22:51:20 -0700

    added comments

--- spyell/crawler.py
+++ spyell/crawler.py
@@ -2,14 +2,17 @@ import requests
 from bs4 import BeautifulSoup
 
 def crawl_url(url):
+
+    # grab web content
     req = requests.get(url)
     
+    # import into bs4
     soup = BeautifulSoup(req.content, 'html')
 
-    
-
+    # grab all p tags
     paragraphs = soup.find_all('p')
 
+    # print out the text in each p tag
     for paragraph in paragraphs:
         print(paragraph.get_text())
 
