21689781ab15226dc0d8b1d292ff971590695009
Avery Biskup <averybiskup@gmail.com>
2022-08-18 22:19:09 -0700

    Added todos

--- selenium/lc.py
+++ selenium/lc.py
@@ -5,4 +5,13 @@ driver.get("https://www.leetcode.com")
 
 sign_in_button = driver.find_element("xpath", '//a[@href="/accounts/login/"]')
 
+# TODO:
+#   complete sign in
+#   navigate to problem types page
+#   gather all problem names 
+#   from all the names, gather all descriptions
+#   create SQL db to store data
+
+
+
 driver.execute_script("arguments[0].click();", sign_in_button);
